cc69d826ca1400376657839d046ec13f0ff51f86
David Hays <dhays90@gmail.com>
2014-05-28 10:22:25 -0500

    t6-common: fix breakfast sync error if no vendor

--- t6-common.mk
+++ t6-common.mk
@@ -189,4 +189,4 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalv
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Include non-opensource parts
-$(call inherit-product, vendor/htc/t6-common/t6-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/t6-common/t6-common-vendor.mk)
