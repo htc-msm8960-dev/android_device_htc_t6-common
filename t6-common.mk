f75dafd82342de3bdcc31fd938ce1228197bd561
David Hays <dhays90@gmail.com>
2014-11-12 23:57:02 -0600

    t6-common: Add WiFi packages and configuration
    Change-Id: I21b51e6fd95b5f39a82f51536c3989ad5c225c60

--- t6-common.mk
+++ t6-common.mk
@@ -121,6 +121,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 # Misc Packages
 PRODUCT_PACKAGES += \
     Torch
