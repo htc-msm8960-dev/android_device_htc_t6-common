0a0df7f5d79aeb413cc7e7f0ee06cc7a4e7afe88
Jurica Vukadin <u.ra604@gmail.com>
2014-11-14 21:08:43 +0100

    Revert "t6-common: Add WiFi packages and configuration"
    This reverts commit f75dafd82342de3bdcc31fd938ce1228197bd561.
    
    Change-Id: Ia8505303efe399b804a049c5079a53b3930bf68b

--- t6-common.mk
+++ t6-common.mk
@@ -121,12 +121,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
-PRODUCT_PACKAGES += \
-    dhcpcd.conf \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf
-
 # Misc Packages
 PRODUCT_PACKAGES += \
     Torch
