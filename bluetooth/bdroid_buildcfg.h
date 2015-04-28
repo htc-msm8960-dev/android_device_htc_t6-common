22cf6463023be2c530c40396b55935465900b267
jrior001 <jriordan001@gmail.com>
2015-04-27 23:37:03 -0400

    t6-common: update bluetooth config
    Change-Id: I33c4245bc74ac41cf3d23c2aa28250cfe2454355

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,6 +19,5 @@
 
 #define BTM_DEF_LOCAL_NAME   "HTC One Max"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BTC_INCLUDED TRUE
+#define BLUETOOTH_QTI_SW TRUE
 #endif
