ced0c33dc863655df86ba3eab293affdf2c30be5
Ethan Chen <intervigil@gmail.com>
2015-06-07 12:12:50 -0700

    t6-common: Convert libaudioamp to audio_amplifier HAL
    Change-Id: I6c111396b2737f3ceeeb90f4ce999e42a3b11599

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,7 +46,6 @@ BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use c
 BOARD_USES_SEPERATED_FM := true # use distinct fm use cases
 BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
 BOARD_USES_SEPERATED_VOIP := true  # use distinct VOIP use cases
-BOARD_AUDIO_AMPLIFIER := device/htc/t6-common/libaudioamp
 BOARD_HAVE_HTC_CSDCLIENT := true
 AUDIO_FEATURE_ENABLED_FM := true
 QCOM_FM_ENABLED := true
