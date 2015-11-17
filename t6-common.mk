bf639a2c41349d16846d7db56d134ae614ee4646
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-17 14:00:28 -0500

    t6-common: Shim libqc-opt rather than hexediting it.
    Change-Id: I9a2e52cccfbd131aabb72f85705496bd36a318a8

--- t6-common.mk
+++ t6-common.mk
@@ -121,6 +121,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh
 
+# PowerHAL
+PRODUCT_PACKAGES += \
+    libqc-opt_shim
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
