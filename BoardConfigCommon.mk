0b4da8f96621ce7058198732fc0e90b0844407c7
David Hays <dhays90@gmail.com>
2016-09-21 10:10:26 -0500

    Revert "t6-common: Build kernel with gcc 4.9"
    N builds with 4.9 by default
    
    This reverts commit d743419a14d49b7603b65ba6f4c8b96a856ec4cd.
    
    Change-Id: I598ad9e5661639788d62761431a7dd5a7e57d129

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=23
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := t6_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Audio
