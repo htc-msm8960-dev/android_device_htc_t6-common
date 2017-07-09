467aa13de54f872e4594f22d985d6c4d042f717c
TARKZiM <tom8476oo@gmail.com>
2017-07-09 16:48:25 +0800

    t6-common: unified device tree

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -8,23 +8,21 @@ LOCAL_SRC_FILES		:= etc/fstab.qcom
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-ifneq ($(filter t6,$(TARGET_DEVICE)),) #GSM
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.gsm
+LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-else #CDMA
+
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE		:= init.target.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.cdma
+LOCAL_SRC_FILES		:= etc/init.target.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
---
+++ rootdir/etc/init.target.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+
+buildvariant='getprop ro.build.device'
+case "$buildvariant" in
+    "t6vzw" | "t6spr")
+        /system/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/
+        ;;
+    *)
+        /system/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/ -u
+        ;;
+esac
