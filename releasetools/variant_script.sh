ea44091c4a6f24fb1dfef2763913aaf37c275b2b
TARKZiM <tom8476oo@gmail.com>
2017-07-10 11:32:22 +0800

    t6-common: update variant scripts

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -10,6 +10,8 @@ case $modelid in
     "0P3P70000") variant="spr" ;;
     "0P3P30000") variant="dug" ;;
     "0P3P20000") variant="dwg" ;;
+    "0P3P52000") variant="gsm"; tw="true" ;;
+	"0P3P51000") variant="gsm"; eu="true" ;;
     *)           variant="gsm" ;;
 esac
 
