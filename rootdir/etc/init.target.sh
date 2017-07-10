f322f6cac38aad4ebd33c830a5bbfde4e8e479a8
TARKZiM <tom8476oo@gmail.com>
2017-07-11 02:28:16 +0800

    t6-common: update init scripts

--- rootdir/etc/init.target.sh
+++ rootdir/etc/init.target.sh
@@ -1,9 +1,12 @@
 #!/system/bin/sh
 
-buildvariant=`getprop ro.product.device`
+buildvariant=`getprop ro.boot.mid`
 case "$buildvariant" in
-    "t6vzw" | "t6spr")
+    "0P3P10000" | "0P3P70000")
         /system/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/
+        ;;
+    "0P3P52000" | "0P3P51000")
+        /system/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/ -u
         ;;
     *)
         /system/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/ -u
