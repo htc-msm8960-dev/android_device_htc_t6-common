946a588a3b88f0dacf02685f344f93b58ba6a8e5
Jia Cheng She <tom8476oo@gmail.com>
2017-07-11 13:56:06 +0800

    t6-common: remove unused lines for releasetools

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -28,5 +28,3 @@ def FullOTA_InstallEnd(info):
 
 def FullOTA_PostValidate(info):
   info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
-  info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
-  info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
