4afb2a96b4572646b82fd201ece57606d032fd59
Jia Cheng She <tom8476oo@gmail.com>
2017-07-12 03:11:04 +0800

    t6-common: add t6tl variant support

--- releasetools/variant_script.sh
+++ releasetools/variant_script.sh
@@ -10,13 +10,14 @@ case $modelid in
     "0P3P70000") variant="spr" ;;
     "0P3P30000") variant="dug" ;;
     "0P3P20000") variant="dwg" ;;
+    "0P3P40000") variant="tl" ;;
     "0P3P52000") variant="gsm"; tw="true" ;;
     "0P3P51000") variant="gsm"; eu="true" ;;
     *)           variant="gsm" ;;
 esac
 
 # Skip symlink creation for Dual SIM variants because blobs are already in the proper location
-if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ]; then
+if [ "$variant" == "vzw" ] || [ "$variant" == "spr" ] || [ "$variant" == "gsm" ] || [ "$variant" == "tl" ]; then
   basedir="/system/blobs/$variant/"
   cd $basedir
   chmod 755 bin/*
