1a7fcf57427803199228c37d4ec64cfa145e482d
tarkzim <tom8476oo@gmail.com>
2017-09-05 20:49:32 +0800

    bluetooth: Build the new Bluetooth HAL

--- t6-common.mk
+++ t6-common.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8960 \
