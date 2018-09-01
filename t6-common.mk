5e6bfb926b510bb3397071f23ce590b9684a541f
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:00 +0200

    t6-common: camera: Specify internal video devices to ignore by the external provider
    The external camera provider occupies our camera V4L2 nodes, potentially
    crashing the kernel driver and blocking the camera HAL.
    Unfortunately, there's no easy way to just disable it, so let's just tell
    it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- t6-common.mk
+++ t6-common.mk
@@ -87,6 +87,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl
 
+# Camera External Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Charger
 WITH_LINEAGE_CHARGER := true
 
