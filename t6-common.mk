1af5da727d22f600cf57ecfffa58a6f6c202ab1d
TARKZiM <tom8476oo@gmail.com>
2019-03-31 09:34:32 +0800

    t6-common: update GPS HAL * from https://github.com/LineageOS/android_device_htc_msm8974-common * based on LA.BF.1.1.3_rb1.13

--- t6-common.mk
+++ t6-common.mk
@@ -113,14 +113,13 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
-    libloc_eng \
-    libloc_core \
-    libgps.utils \
     gps.msm8960
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # IDC
 PRODUCT_COPY_FILES += \
