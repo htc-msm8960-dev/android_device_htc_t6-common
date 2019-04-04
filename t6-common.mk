23759738b1828ce1c0bc197cc6265bbc508e0c6d
TARKZiM <tom8476oo@gmail.com>
2019-04-04 20:49:33 +0800

    t6-common: enable treble OMX

--- t6-common.mk
+++ t6-common.mk
@@ -152,7 +152,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # NFC HAL
 PRODUCT_PACKAGES += \
