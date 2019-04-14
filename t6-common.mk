ffb3066ffcd2af38499cb59bcf0123d11561a108
TARKZiM <tom8476oo@gmail.com>
2019-04-14 14:21:48 +0800

    t6-common: add SoundTrigger HAL 2.0 impl

--- t6-common.mk
+++ t6-common.mk
@@ -65,7 +65,8 @@ PRODUCT_PACKAGES += \
 # Audio HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
