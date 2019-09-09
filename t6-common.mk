5983d98d94b00bd9ca7e330f99b05fb5d9c0295a
TARKZiM <tom8476oo@gmail.com>
2019-09-09 22:04:46 +0800

    t6-common: Switch from thermald to thermal-engine
    * from https://github.com/LineageOS/android_device_samsung_jf-common

--- t6-common.mk
+++ t6-common.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8064.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8064.conf
 
 # WiFi
 PRODUCT_PACKAGES += \
