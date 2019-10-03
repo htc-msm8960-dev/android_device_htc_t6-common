14da3083ccf02cf01c67e940c478c0e305e18b10
Jimmy Chen <jimmycmchen@google.com>
2019-10-03 11:24:23 +0800

    t6-common: Move supplicant service init to wpa_supplicant
    Bug: 138790309
    Test: boot and wifi function is ok
    Change-Id: I35cf3e03566443e20799623045eacd423fd7f5c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,6 +125,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/wlan/parameters/fwpath"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # inherit from the proprietary version
 -include vendor/htc/t6-common/BoardConfigVendor.mk
